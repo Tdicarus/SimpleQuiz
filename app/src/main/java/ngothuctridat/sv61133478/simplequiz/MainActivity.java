15602dd2969ec7632fca6e4a66c8632a4c694764
Tdicarus <ngt679@gmail.com>
2022-05-01 16:23:51 +0700

    a

---
+++ app/src/main/java/ngothuctridat/sv61133478/simplequiz/MainActivity.java
@@ -0,0 +1,13 @@
+package ngothuctridat.sv61133478.simplequiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
